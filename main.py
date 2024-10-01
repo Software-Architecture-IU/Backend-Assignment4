87a77a7b856d1fe78d498d33cbd1862f53bb7ded
Max <ibroden@yandex.ru>
2024-10-01 22:50:49 +0300

    [filters] Implement resize filter.

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from sink import DisplaySink
 from source import WebcamSource
 from filters.dummy import DummyFilter
+from filters.resize import ResizeFilter
 
 from multiprocessing import Queue
 
 dummyInput = Queue()
 sinkInput = Queue()
+resizeInput = Queue()
 
-src = WebcamSource(outputs=[dummyInput])
+src = WebcamSource(outputs=[resizeInput])
 dummy = DummyFilter(input=dummyInput, outputs=[sinkInput])
+resize = ResizeFilter(input=resizeInput, outputs=[sinkInput], sizes=(800, 200))
+
 sink = DisplaySink(input=sinkInput)
 
 if __name__ == '__main__':
     src.start()
     dummy.start()
+    resize.start()
     sink.start()
 
     sink.join()
+    resize.join()
     dummy.join()
     src.join()

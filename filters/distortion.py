338f6dea2ac19a4e24f42f506aa8ea85e1ff1576
m0t9 <matveykorinenko@gmail.com>
2024-10-02 19:57:44 +0300

    impl(distortion): add distortion to main pipeline

--- filters/distortion.py
+++ filters/distortion.py
@@ -3,17 +3,23 @@ from filters.base import BaseFilter
 import cv2
 import numpy as np
 
+from typing import List
+from multiprocessing import Queue
+
 
 class DistortionFilter(BaseFilter):
+    def __init__(self, input: Queue, outputs: List[Queue], k1: float, k2: float):
+        super(DistortionFilter, self).__init__(input=input, outputs=outputs)
+        self.k1 = k1
+        self.k2 = k2
+
     def process(self, frame):
         h, w = frame.shape[:2]
-        k1 = -0.1
-        k2 = -0.1
 
         x, y = np.meshgrid(np.linspace(-1, 1, w), np.linspace(-1, 1, h))
         r = np.sqrt(x ** 2 + y ** 2)
-        x_distorted = x * (1 + k1 * r ** 2 + k2 * r ** 4)
-        y_distorted = y * (1 + k1 * r ** 2 + k2 * r ** 4)
+        x_distorted = x * (1 + self.k1 * r ** 2 + self.k2 * r ** 4)
+        y_distorted = y * (1 + self.k1 * r ** 2 + self.k2 * r ** 4)
 
         map_x = ((x_distorted + 1) * w / 2).astype(np.float32)
         map_y = ((y_distorted + 1) * h / 2).astype(np.float32)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from multiprocessing import Queue
 
 from filters.bnw import BnWFilter
+from filters.distortion import DistortionFilter
 from filters.dummy import DummyFilter
 from filters.mirror import MirrorFilter
 from filters.resize import ResizeFilter
@@ -11,13 +12,15 @@ dummyInput = Queue()
 resizeInput = Queue()
 mirrorInput = Queue()
 bnwInput = Queue()
+distortionInput = Queue()
 sinkInput = Queue()
 
 src = WebcamSource(outputs=[resizeInput])
 dummy = DummyFilter(input=dummyInput, outputs=[resizeInput])
-resize = ResizeFilter(input=resizeInput, outputs=[mirrorInput], sizes=(800, 200))
+resize = ResizeFilter(input=resizeInput, outputs=[mirrorInput], sizes=(400, 400))
 mirror = MirrorFilter(input=mirrorInput, outputs=[bnwInput])
-bnw = BnWFilter(input=bnwInput, outputs=[sinkInput])
+bnw = BnWFilter(input=bnwInput, outputs=[distortionInput])
+distort = DistortionFilter(input=distortionInput, outputs=[sinkInput], k1=-0.2, k2=0.5)
 sink = DisplaySink(input=sinkInput)
 
 if __name__ == '__main__':
@@ -26,10 +29,12 @@ if __name__ == '__main__':
     resize.start()
     mirror.start()
     bnw.start()
+    distort.start()
     sink.start()
 
     sink.join()
 
+    distort.kill()
     src.kill()
     dummy.kill()
     resize.kill()
